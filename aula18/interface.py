b361fe4743c9798fbdfdcfb24d92ec3bc77d2753
Ruan Dias <ruandiasat@gmail.com>
2024-10-19 22:40:16 -0300

    Update interface.py

--- aula18/interface.py
+++ aula18/interface.py
@@ -3,23 +3,23 @@ from abc import ABC, abstractmethod
 class AveVoadora(ABC):
 
     @abstractmethod
-    def comer(self):
+    def comer(self) -> None:
         raise 'Should implement comer method'
 
     @abstractmethod
-    def voar(self):
+    def voar(self) -> None:
         raise 'Should implement voar method'
 
     @abstractmethod
-    def gritar(self):
+    def gritar(self) -> None:
         raise 'Should implement gritar method'
     
 class AveTerrestre(ABC):
 
     @abstractmethod
-    def comer(self):
+    def comer(self) -> None:
         raise 'Should implement comer method'
 
     @abstractmethod
-    def gritar(self):
+    def gritar(self) -> None:
         raise 'Should implement gritar method'
